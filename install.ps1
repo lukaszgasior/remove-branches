7244cacba88e8194c1e254ec19cc3f103d70c90a
Lukasz Gasior <lukasz@gasior.net.pl>
2024-06-23 11:13:56 +0000

    windows install script

--- install.ps1
+++ install.ps1
@@ -2,20 +2,34 @@ param (
     [string]$version = "v0.1.0"
 )
 
+if (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    if ($PSVersionTable.PSEdition -eq "Core") {
+        $shell = "pwsh"
+    }
+    else {
+        $shell = "powershell"
+    }
+
+    $scriptPath = $MyInvocation.MyCommand.Path
+    Start-Process -FilePath $shell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
+    
+    exit
+}
+
 $repo = "lukaszgasior/remove-branches"
-$binName = "git-remove-branches"
+$binName = "remove-branches"
 $os = "Windows"
 $arch = "x86_64"
 $url = "https://github.com/$repo/releases/download/$version/${binName}_${os}_${arch}.zip"
 
 $output = "$env:TEMP\$binName.zip"
-$binPath = "$env:ProgramFiles\Git\bin"
+$binPath = "$env:ProgramFiles\Git\cmd"
 
 Write-Host "Downloading $url"
 Invoke-WebRequest -Uri $url -OutFile $output
 
 Write-Host "Extracting $output"
-Expand-Archive -Path $output -DestinationPath $env:TEMP
+Expand-Archive -Path $output -DestinationPath $env:TEMP -Force 
 
 Write-Host "Installing $binName to $binPath"
 Move-Item -Path "$env:TEMP\$binName.exe" -Destination $binPath -Force
@@ -24,4 +38,7 @@ Write-Host "Cleaning up"
 Remove-Item -Path $output
 
 Write-Host "Installation complete"
+
+Write-Host "Press any key to continue..."
+[System.Console]::ReadKey($true) > $null
 
